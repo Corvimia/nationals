2a66c048f963e82db9ab3250a6256577b87f964b
Thomas Lee <thomaslee@tleembp.home>
2015-03-27 18:32:24 +0000

    Added all latest stuff (I think)

--- model.js
+++ model.js
@@ -6,6 +6,8 @@
 
 	University = (function() {
 		
+		University.prototype.id = -1;
+		University.prototype.name = '';
 		University.prototype.points = 0;
 		
 		function University() {
@@ -19,15 +21,28 @@
 	
 	Competitor = (function() {
 		
-		Competitor.prototype.points = 0;
+		Competitor.prototype.name = '';
+		Competitor.prototype.id = -1;
+		Competitor.prototype.team_name;
 		
 		function Competitor() {
 	
 		
 		}
-
+		
+		Competitor.prototype.find_and_set_name_and_team = function () {
+			var competitor_data = data.get_entry('student', this.id)
+			
+			this.name = competitor_data.name;
+			this.team_name = data.get_entry('university', competitor_data.university_fk).name;
+			
+		};
+		
 		return Competitor;
 
 	})();
+	
+	window.University = University;
+	window.Competitor = Competitor;
 	
 })();
