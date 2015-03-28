6a4dddd7a284b4bcbd67f6d69c6bd3df903fce7b
Thomas Lee <thomaslee@tleembp.home>
2015-03-28 19:28:57 +0000

    Non animated sorting is in place.

--- leaderboard.js
+++ leaderboard.js
@@ -6,13 +6,17 @@
 (function () {
 	new Firebase('https://luminous-torch-1139.firebaseio.com/nationals_test_tom4/score').remove();
 	data.init('nationals_test_tom4');
+	 
+	var space_count = 0, category_list_count = 0;
+	var num_categories;
 	
 	setTimeout(function() {
 		populate_leaderboard();
+		num_categories = data.get_list('category').length;
 		data.init_university_total(uni_callback);
 	}, 2000);
 
-	var space_count = 0, category_list_count = 0;
+	
 	
 	
 	document.onkeypress=function(e){
@@ -28,12 +32,25 @@
 	function uni_callback(uni_details) {
 		console.log(uni_details)
 		var points_table_rows = toArray(document.querySelectorAll('#points-table tbody tr'));
-		
-		points_table_rows.forEach(function (row, index, array) {
+		var i, index = 0;
+		var updated_row = null;
+		points_table_rows.forEach(function (row, tmp_index, array) {
 			if ($(row).data('uid')===uni_details.university_fk) {
+				updated_row = row;
 				row.children[1].innerHTML=uni_details.total;
+				index = tmp_index;
 			}
 		});
+		
+		var row;
+		
+		for (i=0; i < points_table_rows.length; ++i) {
+			row = points_table_rows[i]; 
+			if (uni_details.total >= parseInt(row.children[1].innerHTML, 10)) {
+				$(updated_row).insertBefore(row);
+				return;
+			}
+		}
 		
 	}
 	
@@ -41,6 +58,10 @@
 		var category_section = document.getElementById('category');
 		var table_row = null, table_cells = null;
 		var space_position = space_count % 4
+		
+		if(space_count / 4 > num_categories) {
+			return;
+		}
 		
 		if (space_position > 0) {
 			
@@ -137,7 +158,7 @@
 	}
 	
 	function push_score_data(table_row) {
-		var competitor = $(table_row).data('competitor');
+		var i, competitor = $(table_row).data('competitor');
 		var score = {
 			student_fk: competitor.id, 
 			university_fk:competitor.university_id,
@@ -147,6 +168,7 @@
 		};
 		
 		data.save('score', [score]);
+		
 	}
 	
 	
